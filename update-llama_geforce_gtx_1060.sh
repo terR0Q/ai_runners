5058228ad5c4df65567abe1a8cf8c9057cf7a1c9
Nickolai Parinoff <np@codepeckers.ru>
2026-04-25 20:19:04 +0300

    Updated llama GeForce GTX 1060 build script

--- update-llama_geforce_gtx_1060.sh
+++ update-llama_geforce_gtx_1060.sh
@@ -6,5 +6,5 @@ cmake -B build \
   -DGGML_CUDA=ON \
   -DCMAKE_CUDA_ARCHITECTURES=61 \
   -DGGML_CUDA_FORCE_MMQ=OFF
-cmake --build build --config Release -j --clean-first
+cmake --build build --config Release -j2 --clean-first
 sudo systemctl start llama-server
