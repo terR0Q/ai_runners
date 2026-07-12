f4748fbb842cac211f96751143ddfcd4ef5744b7
Nickolai Parinoff <np@codepeckers.ru>
2026-07-12 22:22:51 +0300

    Updated update-llama_geforce_gtx_3060.sh for gcc 16

--- update-llama_geforce_gtx_3060.sh
+++ update-llama_geforce_gtx_3060.sh
@@ -6,5 +6,6 @@ rm -rf build
 cmake -B build \
   -DGGML_CUDA=ON \
   -DGGML_VULKAN=OFF \
-  -DGGML_HIP=OFF
+  -DGGML_HIP=OFF \
+  -DCMAKE_CUDA_FLAGS="-allow-unsupported-compiler"
 cmake --build build --config Release -j --clean-first
