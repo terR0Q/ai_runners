3e7796caebf71c3276a3acadfb236f3b138955e1
Nickolai Parinoff <np@codepeckers.ru>
2026-07-12 23:41:27 +0300

    Updated update-llama_geforce_gtx_3060.sh for SUSE

--- update-llama_geforce_gtx_3060.sh
+++ update-llama_geforce_gtx_3060.sh
@@ -9,3 +9,7 @@ cmake -B build \
   -DGGML_HIP=OFF \
   -DCMAKE_CUDA_FLAGS="-allow-unsupported-compiler"
 cmake --build build --config Release -j --clean-first
+
+# Fedora / SUSE linux to allow service run:
+sudo semanage fcontext -a -t bin_t "/home/terr0q/loot/ai/llama.cpp/build/bin/llama-server"
+sudo restorecon -v /home/terr0q/loot/ai/llama.cpp/build/bin/llama-server
