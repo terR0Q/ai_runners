dd8c9f4d23ad53d2f92251b17c0cc53fd7ad13b1
Nickolai Parinoff <np@codepeckers.ru>
2026-07-12 21:58:37 +0300

    Added update-llama_geforce_gtx_3060.sh

---
+++ update-llama_geforce_gtx_3060.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo systemctl stop llama-server
+cd llama.cpp
+git pull
+rm -rf build
+cmake -B build \
+  -DGGML_CUDA=ON \
+  -DGGML_VULKAN=OFF \
+  -DGGML_HIP=OFF
+cmake --build build --config Release -j --clean-first
